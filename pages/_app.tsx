f257184f91ed8190ea6b5cee2002edc6d9ab2382
linus <1748501327@qq.com>
2024-06-20 11:03:08 +0800

    save

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,11 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider, useAccount } from 'wagmi';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { createConfig, http } from '@wagmi/core'
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  okxWallet, metaMaskWallet, coinbaseWallet, walletConnectWallet
+} from "@rainbow-me/rainbowkit/wallets";
 import Dymic from './dymic';
 const artela = {
   id: 11822,
@@ -37,15 +42,34 @@ const artela = {
   },
   testnet: false,
 };
-
-const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    artela
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [okxWallet, metaMaskWallet, coinbaseWallet, walletConnectWallet],
+    },
   ],
-  ssr: true,
-});
+  {
+    appName: 'Renaissance',
+    projectId: '080999b35ff4ade22e595b9aeb16db24',
+  }
+);
+
+const config = createConfig({
+  connectors,
+  transports: {
+    [artela.id]: http("https://betanet-rpc1.artela.network")
+  },
+  chains: [artela]
+})
+// const config = getDefaultConfig({
+//   appName: 'RainbowKit App',
+//   projectId: 'YOUR_PROJECT_ID',
+//   chains: [
+//     artela
+//   ],
+//   ssr: true,
+// });
 
 const client = new QueryClient();
 
