1d5a036fe42d6d0ccdebb04cddff5c81a927b343
linus <1748501327@qq.com>
2024-06-20 09:50:53 +0800

    s

--- pages/vision/index.tsx
+++ pages/vision/index.tsx
@@ -3,18 +3,18 @@ import { useMutation } from "@tanstack/react-query";
 import { getNonce } from "./api"
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 const vision = () => {
-    const getNonceQuery = useMutation({
-        mutationFn: () => {
-            return getNonce();
-        },
-        onSuccess: (data) => {
-        }
-    })
+    // const getNonceQuery = useMutation({
+    //     mutationFn: () => {
+    //         return getNonce();
+    //     },
+    //     onSuccess: (data) => {
+    //     }
+    // })
     return (
         <div>
-            <button onClick={() => getNonceQuery.mutate()}>
+            {/* <button onClick={() => getNonceQuery.mutate()}>
                 getNonce
-            </button>
+            </button> */}
             <ConnectButton/>
         </div>
     )
